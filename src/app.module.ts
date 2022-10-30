6caaaed1ba89d38abb4348908b3431d886ef27f6
powdercat <powdercatone@gmail.com>
2022-10-30 22:14:06 +0900

    DB mysql 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import entities from './typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST as string,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME as string,
+      password: process.env.DB_PASSWORD as string,
+      database: process.env.DB_DATABASE as string,
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/typeorm/User.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+    name: 'id', // 데이터베이스 테이블 컬럼명. 이름을 지정하여 변경할 수 있다.
+  })
+  id: number;
+
+  @Column({
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    nullable: false,
+  })
+  password: string;
+}
